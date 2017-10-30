76e605dc549986d498e736142112809c3d0bf996
LIONS\ponsind1 <ponsind1@apps.tcnj.edu>
2017-10-30 11:10:12 -0400

    gave it the right file extension, added comment specifying packages needed

---
+++ Test Code/speechTest.py
@@ -0,0 +1,16 @@
+# make sure to pip install pyaudio and speechrecognition
+
+import speech_recognition as sr
+from naoqi import ALProxy
+
+r = sr.Recognizer()
+tts = ALProxy("ALTextToSpeech", "192.168.1.149", 9559)
+
+with sr.Microphone() as source:
+    print('Say Something!')
+    audio = r.listen(source)
+    print('Done!')
+
+input = r.recognize_google(audio)
+print(input)
+tts.say(str(input))
