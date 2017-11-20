a926618e76fce85fc55561c7d5cfad1d5249ad4d
LIONS\ponsind1 <ponsind1@apps.tcnj.edu>
2017-11-20 11:39:12 -0500

    created python file for controlling head movement

---
+++ Test Code/nao_robot_video_streaming/maybe.py
@@ -0,0 +1,10 @@
+from naoqi import ALProxy
+import almath
+
+def yes(arg, ip):
+    print(arg)
+    argRad = int(arg) * 3.14 / 180
+    moProx = ALProxy("ALMotion", ip, 9559)
+    moProx.setStiffnesses("Head", 1.0)
+    if abs(int(arg)) < 60:
+        moProx.angleInterpolation("HeadYaw", argRad, 1, True)
