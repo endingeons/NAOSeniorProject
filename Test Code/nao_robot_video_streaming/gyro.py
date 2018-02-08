dfbd944fea95599e3ecba0b670e494447b67ff6b
LIONS\ponsind1 <ponsind1@apps.tcnj.edu>
2018-02-08 15:01:11 -0500

    made one form for both gyro values, removed other gyro value of direction we dont use, made gyro movement better (and safer) for nao

--- Test Code/nao_robot_video_streaming/gyro.py
+++ Test Code/nao_robot_video_streaming/gyro.py
@@ -1,10 +1,49 @@
 from naoqi import ALProxy
 import almath
 
-def moveHead(arg, ip):
-    print(arg)
-    argRad = int(arg) * 3.14 / 180
+def moveHead(argZ, argY, ip):
+    print("Y: ")
+    print(argY)
+    print("Z: ")
+    print(argZ)
+    argYRad = int(argY) * 3.14 / 180
+    argZRad = int(argZ) * 3.14 / 180
     moProx = ALProxy("ALMotion", ip, 9559)
     moProx.setStiffnesses("Head", 1.0)
-    if abs(int(arg)) < 60:
-        moProx.angleInterpolation("HeadYaw", argRad, 1, True)
+    names = ["HeadPitch", "HeadYaw"]
+    times = [.5, .5]
+
+    # normal case
+    if (abs(int(argY)) < 25) and (abs(int(argZ)) < 90):
+        angles = [argYRad, argZRad]
+        moProx.angleInterpolation(names, angles, times, True)
+
+    # if y is outside of bounds
+    elif (abs(int(argY)) > 25) and (abs(int(argZ)) < 90):
+        if int(argY) < 0:
+            angles = [-25 * 3.14 / 180, argZRad]
+        elif int(argY) > 0:
+            angles = [25 * 3.14 / 180, argZRad]
+        moProx.angleInterpolation(names, angles, times, True)
+
+    # if z is outside of bounds
+    elif (abs(int(argY)) < 25) and (abs(int(argZ)) > 90):
+        if int(argZ) < 0:
+            angles = [argY, -90 * 3.14 / 180]
+        elif int(argZ) > 0:
+            angles = [argY, 90 * 3.14 / 180]
+        moProx.angleInterpolation(names, angles, times, True)
+
+    # if both are outside of bounds
+    elif (abs(int(argY)) > 25) and (abs(int(argZ)) > 90):
+        if int(argY) < 0 and int(argZ) < 0:
+            angles = [-25 * 3.14 / 180, -90 * 3.14 / 180]
+        elif int(argY) < 0 and int(argZ) > 0:
+            angles = [-25 * 3.14 / 180, 90 * 3.14 / 180]
+        elif int(argY) > 0 and int(argZ) < 0:
+            angles = [25 * 3.14 / 180, -90 * 3.14 / 180]
+        elif int(argY) > 0 and int(argZ) > 0:
+            angles = [25 * 3.14 / 180, 90 * 3.14 / 180]
+        moProx.angleInterpolation(names, angles, times, True)
+
+    moProx.setStiffnesses("Head", 0.0)
