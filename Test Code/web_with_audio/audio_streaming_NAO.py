bc66012b0affe8a9d72ea7e93fd56757083243b8
Chelsea Cantone <cantonc1@tcnj.edu>
2018-03-30 16:57:11 -0400

    Audio code that runs but filtering is not the greatest

--- Test Code/web_with_audio/audio_streaming_NAO.py
+++ Test Code/web_with_audio/audio_streaming_NAO.py
@@ -14,36 +14,34 @@ import sys
 
 import naoqi
 import sounddevice as sd
-import pyaudio
 import numpy as np
-import StringIO
-import time
 import sys
 import Queue as q
-import scipy.io.wavfile as sp
 import pyaudio
-import wave
-import winsound
 import threading
-
+from collections import deque
+from scipy.signal import butter, lfilter
 class SoundReceiverModule(naoqi.ALModule):
     """
     Use this object to get call back from the ALMemory of the naoqi world.
     Your callback needs to be a method with two parameter (variable name, value).
     """
 
-    def __init__( self, strModuleName, strNaoIp, strNaoPort ):
+    def __init__( self, strModuleName, strNaoIp, pport):
         try:
             naoqi.ALModule.__init__(self, strModuleName);
             self.BIND_PYTHON(self.getName(), "callback");
             self.strNaoIp = strNaoIp
-            self.pport = strNaoPort
+            self.pport = pport;
             self.outfile = None
             self.aOutfile = [None]*(4-1) # ASSUME max nbr channels = 4
             self.nSampleRate = 48000 # or sampling rate could be 16000
             self.channelQ = q.Queue()
+            self.audioDeque = deque([])
+            self.audioDeque_receive = deque([])
             self.p = pyaudio.PyAudio()
-
+            self.thread1 = threading.Thread(target= self.play_audio, args=[])
+            self.thread2 = threading.Thread(target= self.play_audio, args=[])
             # Setting up stream for audio output
             fs = self.nSampleRate  # sampling rate, Hz, must be integer
             # for paFloat32 sample values must be in range [-1.0, 1.0]
@@ -51,6 +49,7 @@ class SoundReceiverModule(naoqi.ALModule):
                                  channels=1,
                                  rate=fs,
                                  output=True)
+
         except BaseException, err:
             print( "ERR: abcdk.naoqitools.SoundReceiverModule: loading error: %s" % str(err) );
 
@@ -76,9 +75,11 @@ class SoundReceiverModule(naoqi.ALModule):
         print( "INF: SoundReceiver: stopping..." )
         audioProxy = naoqi.ALProxy( "ALAudioDevice", self.strNaoIp, self.pport );
         audioProxy.unsubscribe( self.getName() );
+        self.thread1.join()
+        self.thread2.join()
         print( "INF: SoundReceiver: stopped!" )
-        if( self.outfile != None ):
-            self.outfile.close();
+        # if( self.outfile != None ):
+        #     self.outfile.close();
         self.stream.close()
 
     def processRemote( self, nbOfChannels, nbrOfSamplesByChannel, aTimeStamp, buffer):
@@ -102,144 +103,140 @@ class SoundReceiverModule(naoqi.ALModule):
         # numrows = len(aSoundData)
         # numcols = len(aSoundData[0])
         # print("Rows:", numrows, "\tCols:", numcols)
-        inputdata = aSoundData[0,:]
-        self.channelQ.put(inputdata) # Add data from this channel to the queue
-        # print(self.channelQ.qsize())
-        # print("", file=sys.stderr)
-        # threading.Thread(target=sd.play(aSoundData[0], 48000, blocking=False))
-        # print( "len data: %s " % len( self.aSoundData ) );
-        # print( "len data 0: %s " % len( self.aSoundData[0] ) );
-        if( False ):
-            # compute average
-            aAvgValue = np.mean( aSoundData, axis = 1 );
-            print( "avg: %s" % aAvgValue );
-        if( False ):
-            # compute fft
-            nBlockSize = nbrOfSamplesByChannel;
-            signal = aSoundData[0] * np.hanning( nBlockSize );
-            aFft = ( np.fft.rfft(signal) / nBlockSize );
-            print ("%s",aFft);
-        if( False ):
-            # compute peak
-            aPeakValue = np.max( aSoundData );
-            if( aPeakValue > 16000 ):
-                print( "Peak: %s" % aPeakValue );
-        if( False ):
-            bSaveAll = False; #True
-            # save to file
-            if( self.outfile == None ):
-                strFilenameOut = "../nao_robot_video_streaming/test.wav"; # CHANGE THIS
-                print( "INF: Writing sound to '%s'" % strFilenameOut );
-                self.outfile = open( strFilenameOut, "wb" );
-                if( bSaveAll ):
-                    for nNumChannel in range( 1, nbOfChannels ):
-                        strFilenameOutChan = strFilenameOut.replace(".wav", "_%d.wav"%nNumChannel);
-                        self.aOutfile[nNumChannel-1] = open( strFilenameOutChan, "wb" );
-                        print( "INF: Writing other channel sound to '%s'" % strFilenameOutChan );
 
-            # sf.write(strFilenameOut, aSoundDataInterlaced, self.nSampleRate, subtype=None, endian=None, format='WAV', closefd=True)
-            # aSoundDataInterlaced.tofile( self.outfile ); # wrote the 4 channels
-            # aSoundData[0].tofile( self.outfile ); # wrote only one channel
-            # print( "aTimeStamp: %s" % aTimeStamp );
-            # print( "data written: " ),
-            # for i in range( 8 ):
-            #     print( "%d, " % (self.aSoundData[0][i]) ),
-            # print( "" );
-            #self.stop(); # make naoqi crashes
-            if( bSaveAll ):
-                for nNumChannel in range( 1, nbOfChannels ):
-                    self.aSoundData[nNumChannel].tofile( self.aOutfile[nNumChannel-1] );
+        # filter audio data
+        # ****************** bandpass
+        lowcut = 400 #don't go below 150, sweet spot is 300
+        highcut = 3000 #don't do 1000
+        y = self.butter_bandpass_filter(aSoundData[0,:], lowcut, highcut, self.nSampleRate, order=6)
+        inputdata = y
+
+        # ***************** lowpass
+        # cutoff = 1000
+        # y = self.butter_lowpass_filter(self, aSoundData[0,:], cutoff, self.nSampleRate, order=5)
+        # inputdata = y
+
+        # inputdata = aSoundData[0,:]
+        self.audioDeque.append(inputdata)
+        # self.channelQ.put(inputdata) # Add data from this channel to the queue
+
     # processRemote - end
+
+    # Create filter functions
+    def butter_lowpass(self, cutoff, fs, order):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low')
+        return b, a
+
+    def butter_lowpass_filter(self, data, cutoff, fs, order):
+        b, a = self.butter_lowpass(cutoff, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    def butter_bandpass(self, lowcut, highcut, fs, order):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+
+        b, a = butter(order, [low, high], btype='band')
+        return b, a
+
+    def butter_bandpass_filter(self, data, lowcut, highcut, fs, order):
+        b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    def play_audio(self):
+        volume = 0.9
+        while True:
+            while(len(self.audioDeque_receive) != 0):
+                    samples = self.audioDeque_receive.popleft()
+                    sd.play(samples, self.nSampleRate, blocking= False)
+                    # self.stream.write(volume * samples)
+    # play_audio - end
 
     def get_audio(self):
         print("Playing:", file=sys.stderr)
-        time.sleep(5)
+        self.thread1.start()
+        self.thread2.start()
+        # time.sleep(5)
         while(True):
-            if(self.channelQ.qsize() > 4):
-                print(self.channelQ.qsize())
-                print("", file=sys.stderr)
+            if(len(self.audioDeque) > 11):
+                # print(self.channelQ.qsize())
+                # print("", file=sys.stderr)
                 volume = 0.9  # range [0.0, 1.0]
 
                 # generate samples, note conversion to float32 array
-                samples1 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                samples2 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                samples3 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                samples4 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                samples5 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                # samples6 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                # samples7 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                # samples8 = self.channelQ.get().astype(np.float32, order='C') / 32768.0;
-                samples = np.concatenate((samples1, samples2, samples3, samples4, samples5));
+                # samples1 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples2 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples3 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples4 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples5 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples6 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples7 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+                # samples8 = self.channelQ.get().astype(np.float32, order='C') / 32768.0
+
+                samples1 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples2 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples3 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples4 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples5 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples6 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples7 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples8 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples9 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples10 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples11 = self.audioDeque.popleft().astype(np.float32, order='C') / 32768.0
+                samples = np.concatenate((samples1, samples2, samples3, samples4, samples5, samples6, samples7, samples8, samples9, samples10, samples11))
                 # play. May repeat with different volume values (if done interactively)
-                self.stream.write(volume * samples)
-                # stream.stop_stream()
-                # stream.close()
-                # p.terminate()
-
-                # p = pyaudio.PyAudio()
-                #
-                # volume = 0.5  # range [0.0, 1.0]
-                # fs = 44100  # sampling rate, Hz, must be integer
-                # duration = 1.0  # in seconds, may be float
-                # f = 440.0  # sine frequency, Hz, may be float
-                #
-                # # generate samples, note conversion to float32 array
-                # samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
-                #
-                # # for paFloat32 sample values must be in range [-1.0, 1.0]
-                # stream = p.open(format=pyaudio.paFloat32,
-                #                 channels=1,
-                #                 rate=fs,
-                #                 output=True)
-                #
-                # # play. May repeat with different volume values (if done interactively)
-                # stream.write(volume * samples)
-                #
-                # stream.stop_stream()
-                # stream.close()
-                #
-                # p.terminate()
-
-
-                # buf = StringIO.StringIO()
-                # threading.Thread(target=sd.play(self.channelQ.get(), self.nSampleRate)).start()
-                # channel_1 = self.channelQ.get() #Dequeue from our data from channel 0
-                # sd.play(self.channelQ.get(), self.nSampleRate)
-                # Write our channel data into a .WAV file format
-                # Use the StringIO buf to mimic a file stream
-                # sp.write(buf, 48000, channel_1) #Input data from the channel is a 16-bit integer
-                # wav = buf.getvalue()
-                # print("Playing:", file=sys.stderr)
-                # chunk = 1024
-                # wf = wave.open(buf, 'rb')
-                # p = pyaudio.PyAudio()
-                #
-                # stream = p.open(
-                #     format=p.get_format_from_width(wf.getsampwidth()),
-                #     channels=wf.getnchannels(),
-                #     rate=wf.getframerate(),
-                #     output=True)
-                # data = wf.readframes(chunk)
-                #
-                # while data != '':
-                #     stream.write(data)
-                #     data = wf.readframes(chunk)
-                #
-                # stream.close()
-                # p.terminate()
-                #
-                # return wav
-                # print(channel_1, file=sys.stderr);
-                # size = len(channel_1)
-                # for i in range(0, size):
-                #     print(channel_1[i], file=sys.stderr)
-                # dataSize = len(channel_1)
-                # print(dataSize, file=sys.stderr)
-                #sd.play(channel_1, 48000, blocking=True)
+                self.audioDeque_receive.append(samples)
+                # sd.play(samples, self.nSampleRate)
+                # sd.wait()
+                # sd.stop()
+                # self.stream.write(volume * samples)
+                # self.stream.stop_stream()
+                # self.stream.close()
+                # self.p.terminate()
             # else:
-            #     # print(self.channelQ.qsize())
-            #     print("nothing", file=sys.stderr)
-
+            #     continue
+        # # Poll for queue size
+        # while(self.channelQ.qsize() < 8):
+        #     continue
+        #
+        # samples1 = self.channelQ.get().astype(np.int16, order='C')
+        # samples2 = self.channelQ.get().astype(np.int16, order='C')
+        # samples3 = self.channelQ.get().astype(np.int16, order='C')
+        # samples4 = self.channelQ.get().astype(np.int16, order='C')
+        # samples5 = self.channelQ.get().astype(np.int16, order='C')
+        # samples6 = self.channelQ.get().astype(np.int16, order='C')
+        # samples7 = self.channelQ.get().astype(np.int16, order='C')
+        # samples8 = self.channelQ.get().astype(np.int16, order='C')
+        # channel_1 = np.concatenate((samples1, samples2, samples3, samples4, samples5, samples6, samples7, samples8))
+        # print(channel_1)
+        # #https://stackoverflow.com/questions/33879523/python-how-can-i-generate-a-wav-file-with-beeps
+        #
+        # # Dequeue from our data from channel 0
+        # # channel_1 = self.channelQ.get()
+        #
+        # # Use the StringIO buf to mimic a file stream
+        # bytestream = io.BytesIO()
+        # buf = StringIO.StringIO()
+        # wav_file = wave.open(bytestream, "w")
+        # nchannels = 1
+        # sampwidth = 2
+        # nframes = len(channel_1)
+        # comptype = "NONE"
+        # compname = "not compressed"
+        # wav_file.setparams((nchannels, sampwidth, self.nSampleRate, nframes, comptype, compname))
+        #
+        # # Write our channel data into a .WAV file format
+        # # Input data from the channel is a 16-bit integer
+        # for sample in channel_1:
+        #     wav_file.writeframes(struct.pack('h', int(sample)))
+        # # wav = buf.getvalue()
+        # wav_file.close()
+        # return bytestream
 
     def version( self ):
         return "0.6";
